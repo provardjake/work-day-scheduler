2abdbfcd01252a317e16961938d97e978da41a0d
Jake Provard <provardjake@gmail.com>
2023-07-21 21:02:50 -0600

    added clear button and fixed local storage bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,9 +12,11 @@ $(function () {
   var hourSixteen = $("#hour-16");
   var hourSeventeen = $("#hour-17");
   var saveBtn = $(".saveBtn");
+  var clearBtn = $(".clearBtn");
   var hourArray = [hourNine, hourTen, hourEleven, hourTwelve,
   hourThirteen, hourFourteen, hourFifteen, hourSixteen, hourSeventeen];
-  var hourTextArray = [];
+  var textContentArray = [];
+  var hourIdArray = [];
 
 
   //gets the current date and displays it on the page
@@ -26,27 +28,38 @@ $(function () {
   //When the save button is clicked, saves the entered text to an array in local storage
   saveBtn.on("click", function(event){
     event.preventDefault();
-    var hourText = {
-      textContent: $(this).prev().val(), 
-      id: $(this).parent().attr("id")
-    };
 
-    // this loop checks if a saved object already exists. if it does,
-    // replace it with the new input. 
-    for(var i = 0; i < hourTextArray.length; i++){
-      if (hourTextArray[i].id === hourText.id){
-        hourTextArray.splice(i, 1);
+    var textContent = $(this).prev().val();
+    var hourId = $(this).parent().attr("id");
+
+    textContentArray = JSON.parse(localStorage.getItem("textContentArray") || "[]");
+    hourIdArray = JSON.parse(localStorage.getItem("hourIdArray") || "[]");
+
+    if(!textContentArray === null){
+      // This loop checks if a time block already has something stored. if it does, it gets replaced by the new text
+      for(var i = 0; i < hourIdArray.length; i++){
+        if (hourIdArray[i] === hourId){
+          hourIdArray.splice(i, 1);
+          textContentArray.splice(i, 1);
+        }
       }
     }
 
-    hourTextArray.push(hourText);
+    textContentArray.push(textContent);
+    hourIdArray.push(hourId);
 
-    localStorage.setItem("hourTextArray", JSON.stringify(hourTextArray));
+    localStorage.setItem("textContentArray", JSON.stringify(textContentArray));
+    localStorage.setItem("hourIdArray", JSON.stringify(hourIdArray));
 
     renderHourText();
-  })
+  });
 
-  
+  // clears the local storage and refreshes the page
+  clearBtn.on("click", function(){
+    localStorage.clear();
+    window.location.reload();
+  });
+
   // this function gets the current time and compares it to the data-hour of each element. 
   // gives styling based on the comparison
   function getTimeOfDay(){
@@ -74,13 +87,22 @@ $(function () {
 
   //this function displays the stored text to each of the time blocks
   function renderHourText(){
-    var storedHourTextArray = JSON.parse(localStorage.getItem("hourTextArray"));
+    var storedHourIdArray = JSON.parse(localStorage.getItem("hourIdArray"));
+    var storedTextContentArray = JSON.parse(localStorage.getItem("textContentArray"));
 
-    for(var i = 0; i < hourArray.length; i++){
-      if(hourArray[i].attr("id") == storedHourTextArray[0].id){
-        hourArray[i].children("textarea").text(storedHourTextArray[i].textContent);
+    if(storedHourIdArray === null || storedHourIdArray === undefined){
+      return;
+    }
+    else{
+      for(var i = 0; i < hourArray.length; i++){
+        for(var x = 0; x < storedHourIdArray.length; x++){
+          if(storedHourIdArray[x] === hourArray[i].attr("id")){
+            hourArray[i].children(".description").text(storedTextContentArray[x]);
+          }
+        }
       }
     }
+
   }
 
   renderHourText();
